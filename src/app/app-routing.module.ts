e300be3f93918a1839a33b498a3a2b5fbd4f3f07
Markus Kollers <m.kollers@vertical.de>
2021-01-31 02:10:39 +0100

    Move landing page to lazy loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/landing-page/landing-page.module').then(m => m.LandingPageModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { CONFIG, Config } from 'src/config';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LandingPageComponent {
+  constructor(@Inject(CONFIG) public config: Config) {
+  }
+
+}
