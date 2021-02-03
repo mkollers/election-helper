a6c59c6896778e35682901ba32898e1da54521f2
Markus Kollers <m.kollers@vertical.de>
2021-02-03 21:20:28 +0100

    Add footer links

---
+++ src/app/shared/layout/layout.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FooterComponent
+  ]
+})
+export class LayoutModule { }
